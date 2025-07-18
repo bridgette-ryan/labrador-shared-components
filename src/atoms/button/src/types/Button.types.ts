a6783b76a40ac3450261fdca56db91d5000602e9
Bridgette Ryan <bridgetteryan3@gmail.com>
2025-07-18 21:01:00 +0800

    fix: updated button with shared props      removed extra 'script' section in aria-props package.json file

--- src/atoms/button/src/types/Button.types.ts
+++ src/atoms/button/src/types/Button.types.ts
@@ -1,7 +1,13 @@
-export type ButtonProps = {
+import { AriaProps } from "@labrador-sgd/aria-props";
+import { DefaultProps } from "@labrador-sgd/default-props";
+import { InteractiveProps } from "@labrador-sgd/interactive-props";
+import { PropsWithChildren } from "react";
+
+export type ButtonProps = DefaultProps & AriaProps & InteractiveProps & PropsWithChildren & {
     label?: string,
     variant?: string,
     icon?: string,
+    svgSource?: string,
 };
 
 export default ButtonProps;
