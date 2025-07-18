3bf95324caac709a59f2bee2dfc199dc1bfc2d7f
Bridgette Ryan <bridgetteryan3@gmail.com>
2025-07-18 21:14:57 +0800

    feat: storybook init

---
+++ storybook/src/stories/atoms/Button.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import React from "react";
+import { Button } from "@labrador-sgd/button";
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  render: () => (
+    <Button>
+      This is a test
+    </Button>
+  ),
+};
