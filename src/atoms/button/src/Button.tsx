8341510f92366e56d611a50fb77eef46ef06166a
Bridgette Ryan <bridgetteryan3@gmail.com>
2025-07-18 14:01:19 +0800

    fix: added missing react import in Button.tsx

--- src/atoms/button/src/Button.tsx
+++ src/atoms/button/src/Button.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import { Button as BootstrapButton } from 'react-bootstrap';
-
 
 export const Button = () => {
     return <BootstrapButton variant='primary'></BootstrapButton>;
